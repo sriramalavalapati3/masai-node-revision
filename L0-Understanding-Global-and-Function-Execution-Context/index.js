bfddeaf5b14285b137525830d080998b1226fd51
Sriram Alavalapati <sriramalavalapatiit01@gmail.com>
2025-07-23 12:32:47 +0530

    L0-Understanding-Global-and-Function-Execution-Context-day-3-cp-3

---
+++ L0-Understanding-Global-and-Function-Execution-Context/index.js
@@ -0,0 +1,14 @@
+var age = 30;
+
+function DisplayAge() {
+    console.log("Age is: " + age);
+}
+
+function UpdateAge(newAge) {
+    age = newAge;
+    console.log("Age updated to: " + age);
+}
+
+DisplayAge(); // Outputs: Age is: 30
+UpdateAge(35); // Outputs: Age updated to: 35
+DisplayAge(); // Outputs: Age is: 35
